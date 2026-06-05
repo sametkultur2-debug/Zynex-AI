b208cf32ddb2ffd91c3f49dc86c00fc8b80204ca
sametkultur2-debug <sametkultur2@gmail.com>
2026-06-05 16:43:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,3 +10,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server çalışıyor");
 });
+app.get("/api", (req, res) => {
+    res.json({ status: "ok", message: "API çalışıyor" });
+});

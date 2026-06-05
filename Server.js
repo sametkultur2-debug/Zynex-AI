62db2cc91c1784c0c2d9549735adba0d3f1117ec
sametkultur2-debug <sametkultur2@gmail.com>
2026-06-05 13:18:23 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -9,3 +9,26 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server çalışıyor");
 });
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("AI API aktif 🚀");
+});
+
+app.post("/chat", (req, res) => {
+    const message = req.body.message;
+
+    res.json({
+        reply: "Sen dedin: " + message
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server çalışıyor");
+});
